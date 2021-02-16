9cb0bc432783ec727293c2032067751f1421f833
Airi <61052022+Airiuwu@users.noreply.github.com>
2021-02-16 16:22:33 -0500

    its cleanup time

--- kaiyo.js
+++ kaiyo.js
@@ -1,16 +1,15 @@
-var start = +new Date();
 require('events').EventEmitter.prototype._maxListeners = 100;
-
-const config = require("./config.json");
-const Discord = require("discord.js");
-const client = new Discord.Client({autoReconnect: true})
-const token = config.bot.token;
-const guildID = config.bot.guild_id;
+var start = +new Date();
+var config = require("./config.json");
+var Discord = require("discord.js");
+var client = new Discord.Client({autoReconnect: true})
+var token = config.bot.token;
+var guildID = config.bot.guild_id;
 
 require("./constants/commands")(client)
 
 client.on('ready', () => {
-    const guild = client.guilds.get(guildID);
+    var guild = client.guilds.get(guildID);
     console.clear();
     console.log('\n\x1b[36m%s\x1b[0m', ` ██╗  ██╗ █████╗ ██╗██╗   ██╗ ██████╗`);
     console.log('\x1b[34m%s\x1b[0m', ` ██║ ██╔╝██╔══██╗██║╚██╗ ██╔╝██╔═══██╗`);
