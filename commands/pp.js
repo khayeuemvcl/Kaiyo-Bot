2d0aaf68f5630bbe923da71977194113d9540388
Airi <61052022+Airiuwu@users.noreply.github.com>
2021-02-16 16:12:32 -0500

    forgot its supposed to be universal

--- commands/pp.js
+++ commands/pp.js
@@ -1,10 +1,12 @@
 let Discord = require('discord.js');
+let config = require("../config.json")
+let prefix = config.bot.prefix;
 let { ppSizes } = require("../constants/const")
 let penis = ppSizes[Math.round(Math.random() * (ppSizes.length - 1))];
 
 module.exports = function (client) {
     client.on("message", async message => {
-        if (message.content === '>pp') {
+        if (message.content === prefix + 'pp') {
             var embed = new Discord.RichEmbed()
             .setTitle('PEEPEE SIZE MACHINE')
             .addField(message.author.username + "'s pp size is:", "8" + penis + "D")
