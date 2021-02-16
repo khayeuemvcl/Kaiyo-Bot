ed1e3da77d130092ca18cb6e8e4fb7142acab317
Airi <61052022+Airiuwu@users.noreply.github.com>
2021-02-16 16:08:27 -0500

    i like this more lol

--- commands/pp.js
+++ commands/pp.js
@@ -1,21 +1,15 @@
-const Discord = require('discord.js');
-const config = require("../config.json")
-const prefix = config.bot.prefix;
-const { ppSizes, messages } = require("../constants/const")
+let Discord = require('discord.js');
+let { ppSizes } = require("../constants/const")
+let penis = ppSizes[Math.round(Math.random() * (ppSizes.length - 1))];
 
 module.exports = function (client) {
     client.on("message", async message => {
-        if (message.content.startsWith(prefix + 'pp')) {
-            var penis = ppSizes[Math.round(Math.random() * (ppSizes.length - 1))];
-            try {
-                const embed = new Discord.RichEmbed()
-                .setTitle(`PEEPEE SIZE MACHINE`)
-                .addField(`${message.author.username}'s pp size is:`, `8${penis}D`)
-                .setColor('#00ffb3')
-                message.channel.send({embed})
-            } catch (e) {
-                return message.channel.send("pp command broke")
-            }
+        if (message.content === '>pp') {
+            var embed = new Discord.RichEmbed()
+            .setTitle('PEEPEE SIZE MACHINE')
+            .addField(message.author.username + "'s pp size is:", "8" + penis + "D")
+            .setColor('#00ffb3')
+            message.channel.send(embed)
         }
     })
 }
