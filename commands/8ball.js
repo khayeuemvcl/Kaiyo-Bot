53be12b821e55ae36843921f0d25cde1a945666a
Airi <61052022+Airiuwu@users.noreply.github.com>
2021-03-27 01:32:24 -0400

    SHEEEEEEEEEEEEESH

--- commands/8ball.js
+++ commands/8ball.js
@@ -6,17 +6,13 @@ const { responses } = require("../constants/const")
 module.exports = function (client) {
     client.on("message", async message => {
         if (message.content.startsWith(prefix + "8ball")) {
-         if (!message.content.endsWith("?")) return message.channel.send("Please ask a question.")
+        if (!message.content.endsWith("?")) return message.channel.send("Please ask a question.")
         var response = responses[Math.round(Math.random() * (responses.length - 1))];
-			try {
             const embed = new Discord.RichEmbed()
             .setTitle(`${message.author.username} asked: ` + message.content.slice(7))
             .setColor('#00ffb3')
             .setDescription(":8ball: | " + response)
             message.channel.send(embed)
-			}catch (e) {
-				return message.channel.send("a problem has occured, please contact a staff member!")
-			}
         }
     })
 }
